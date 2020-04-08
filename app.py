cf733eca8bd2e8626e6f645ebdb06bca1fa888ed
Marc Schwarze <marc.schwarze@posteo.de>
2020-04-08 16:21:36 +0200

    "Fixed not importing db when running the app."

--- app.py
+++ app.py
@@ -9,11 +9,10 @@ from resources.user import UserRegistration
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
-from db import db
 
 app = Flask(__name__)
 
-app.config['DEBUG'] = True
+# app.config['DEBUG'] = True
 
 # Erzeugt 'data.db' im Wurzelverzeichnis des Projekts.
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
@@ -37,11 +36,7 @@ api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegistration, '/register')
 
 if __name__ == "__main__":
+    from db import db
     db.init_app(app)
-
-    if app.config['DEBUG']:
-        @app.before_first_request
-        def create_tables():
-            db.create_all()
 
     app.run(port=5000, debug=True)
