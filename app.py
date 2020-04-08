2e57bb702e01bf5dda6d8c33119a12174735dc9d
Marc Schwarze <marc.schwarze@posteo.de>
2020-04-08 16:10:08 +0200

    Add debug-mode.

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -10,19 +12,18 @@ from resources.store import Store, StoreList
 from db import db
 
 app = Flask(__name__)
+
+app.config['DEBUG'] = True
+
 # Erzeugt 'data.db' im Wurzelverzeichnis des Projekts.
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    'DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 
 # Anstatt die API "von Hand" zu definieren, wie in den vorherigen Beispielen,
 # wird hier die "Api" von Flask_RESTful genutzt.
 api = Api(app)
-
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 
 jwt = JWT(app, authenticate, identity)  # new endpoint /auth
@@ -37,4 +38,10 @@ api.add_resource(UserRegistration, '/register')
 
 if __name__ == "__main__":
     db.init_app(app)
+
+    if app.config['DEBUG']:
+        @app.before_first_request
+        def create_tables():
+            db.create_all()
+
     app.run(port=5000, debug=True)
